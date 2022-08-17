30bca92d9c7567c3040a5d233ba60d2917a4ef44
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-08-17 16:42:06 +0300

    add name, add number

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import { PageTitle } from 'components/PageTitle/PageTitle.styled';
 import { Container, SectionTitle } from './App.styled';
+import { Contacts } from 'components/Contacts/Contacts';
+import { nanoid } from 'nanoid';
 
 
 
@@ -8,7 +10,32 @@ export class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
+    filter:''
   };
+
+  handleChange = (e) => {   
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(e.target.elements.name.value);
+    // console.log(e.target.elements.number.value);
+    this.setState(prevState => {
+      // console.log(prevState.contacts);      
+      prevState.contacts.push({
+        id: nanoid(),
+        name: e.target.elements.name.value,
+        number: e.target.elements.number.value
+      });
+    });
+  }
+  handleDeleteContact = (id) => {
+    console.log('id', id);
+  }
    
   render() {
     
@@ -16,27 +43,32 @@ export class App extends Component {
     <PageTitle>goit react hw 02 phonebook</PageTitle>
       <Container>
         <SectionTitle>Phonebook</SectionTitle>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>Name
             <input
               type="text"
               name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
             />
           </label>
-          <label>Number
-            <input
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </label>          
-          <button type='button'>add contact</button>
+          <input
+  type="tel"
+            name="number"
+            value={this.state.number}
+            onChange={this.handleChange}
+  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+  required
+/>
+          <button type='submit'>add contact</button>
         </form>
+        <SectionTitle>Contacts</SectionTitle>
+        <Contacts contacts={this.state.contacts} onDelete={this.handleDeleteContact} />         
+        
       </Container>
       </>
     );
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,13 @@
+import { ContactsList } from "./Contacts.styled";
+
+export const Contacts = ({ contacts, onDelete }) => {
+    return (
+        <ContactsList>
+            {contacts.map((item) => {
+                return <li key={item.id}>
+                    <p>{item.name}: {item.number}</p>
+                    <button type="button" onClick={onDelete(item.id)}>x</button>
+                </li>
+            })}
+            </ContactsList>)
+}
